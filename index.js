c86f3f7b0d46408220ed38ebfec1c3a942fc94ab
Matteo Maronati <me@mattinthe.net>
2018-07-05 00:29:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function HtmlWebpackLayoutPlugin (options) {}
 
 HtmlWebpackLayoutPlugin.prototype.apply = function (compiler) {
   var that = this;
-  compiler.plugin('compilation', function(compilation) {
+  compiler.hooks.compilation.tap('html-webpack-layout-plugin', (compilation) => {
     compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('html-webpack-layout-plugin', (htmlPluginData, callback) => {
       htmlPluginData.html = that.addLayout( htmlPluginData.html , htmlPluginData.plugin.options);
       callback(null, htmlPluginData);
@@ -20,7 +20,7 @@ HtmlWebpackLayoutPlugin.prototype.addLayout = function ( html, options ){
   if(options.layout){
       var replace = options.replace || 'content';
       var layout = fs.readFileSync(options.layout, 'utf-8');
-      var reg = new RegExp('{{'+ replace +'}}');
+      var reg = new RegExp(replace);
       html = layout.replace(reg, html);
   }
 
